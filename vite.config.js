a5b2f328e5d34e65ede60d7ddb57043984721424
Umar <umarsheik8610@gmail.com>
2025-02-22 11:51:22 +0530

    added vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/nike-app-using-tailwind-and-react",
+  base: "/nike-landing-page-using-react-and-tailwind",
 });
